e14bd39b8fe3d9b6e65b0e32a9cf55e1a5840041
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-09-25 12:26:26 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan2-DeklarasiVariabel/src/if1/pkg10118038/latihan2/deklarasivariabel/IF110118038Latihan2DeklarasiVariabel.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * Deklarasi dan Variabel
+
+ */
+public class IF110118038Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
